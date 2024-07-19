9ee9d38d31bedce179f0c73bcde990587a8471be
万雅虎 <vipwan@outlook.com>
2024-07-19 13:34:26 +0800

    nuget

--- Microsoft.Extensions.ServiceDiscovery/HostingExtensions.cs
+++ Microsoft.Extensions.ServiceDiscovery/HostingExtensions.cs
@@ -2,23 +2,8 @@
 
 namespace Microsoft.Extensions.Hosting
 {
-
     public static class HostingExtensions
     {
-        /// <summary>
-        /// AddConsulServiceEndPointResolver
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-
-        [Obsolete("Use AddConsulServiceEndpointProvider instead")]
-        public static IServiceCollection AddConsulServiceEndPointResolver(this IServiceCollection services)
-        {
-            services.AddServiceDiscoveryCore();
-            services.AddSingleton<IServiceEndpointProviderFactory, ConsulServiceEndPointProviderFactory>();
-            return services;
-        }
-
         /// <summary>
         /// AddConsulServiceEndpointProvider
         /// </summary>
